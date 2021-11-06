018dc84f95bc021e1661f6aa71db49672b666fa5
Vikas Kumar <vsrathod39@gmail.com>
2021-11-06 15:27:04 +0530

    navbar and huberbergMenu completed

--- components/navbar.js
+++ components/navbar.js
@@ -1,68 +1,86 @@
 function navbar(){
-    return `<nav>
+    return `
     <section id="brandLogo">
-        <img src="https://internshala.com/static/images/common/new_internshala_logo.svg" alt="brandLogo">
-    </section>
-    <section id="services">
-        <ul>
-            <li>
-                <span>Internships</span>
-                <ul>
-                    <li>
-                        <p class="spanTag">Location</p>
-                        <ul>
-                            <li>Work From Home</li>
-                            <li>Internship in Bangalore</li>
-                            <li>Internship in Delhi</li>
-                            <li>Internship in Pune</li>
-                        </ul>
-                    </li>
-                    <li>
-                        <p class="spanTag">Profile</p>
-                        <ul>
-                            <li>Computer Science Internship</li>
-                            <li>Marketing Internship</li>
-                            <li>Finance Internship</li>
-                        </ul>
-                    </li>
-                    <li>
-                        <p class="spanTag">Category</p>
-                        <ul>
-                            <li>Engineering Internship</li>
-                            <li>MBA Internship</li>
-                            <li>Part-Time Jobs/Internship</li>
-                        </ul>
-                    </li>
-                </ul>
-            </li>
-        </ul>
-        <ul>
-            <li>
-                <span>Online Trainings</span><span class="hotText">OFFER</span>
-                <ul>
-                    <li>Programming</li>
-                    <li>Businesss & Management</li>
-                    <li>Data Science</li>
-                    <li>Language</li>
-                </ul>
-            </li>
-        </ul>
-        <ul>
-            <li>Fresher Jobs</li>
-        </ul>
-        <ul>
-            <li><button id="logIn_button">Login</button></li>
-        </ul>
-        <ul>
-            <li>
-                <span id="register_button">Register</span>
-                <ul>
-                    <li>As a student</li>
-                    <li>As an employer</li>
-                </ul>
-            </li>
-        </ul>
-    </section>
-</nav>`
+            <img id="huberbergMenu" src="https://internshala.com/static/images/icons/hamburger-menu-key-new.svg" alt="menu">
+            <img src="https://internshala.com/static/images/common/new_internshala_logo.svg" alt="brandLogo">
+        </section>
+        <section id="services">
+            <ul>
+                <li>
+                    <span>Internships<img id="upDownArrow" src="../images/icons/down-arrow-solid-24.png" alt="dropDownArrow"></span>
+                    <ul>
+                        <li>
+                            <p class="spanTag">Location</p>
+                            <ul>
+                                <li>Work From Home</li>
+                                <li>Internship in Bangalore</li>
+                                <li>Internship in Delhi</li>
+                                <li>Internship in Pune</li>
+                            </ul>
+                        </li>
+                        <li>
+                            <p class="spanTag">Profile</p>
+                            <ul>
+                                <li>Computer Science Internship</li>
+                                <li>Marketing Internship</li>
+                                <li>Finance Internship</li>
+                            </ul>
+                        </li>
+                        <li>
+                            <p class="spanTag">Category</p>
+                            <ul>
+                                <li>Engineering Internship</li>
+                                <li>MBA Internship</li>
+                                <li>Part-Time Jobs/Internship</li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <span>Online Trainings</span><span class="hotText">OFFER</span><img id="upDownArrow" src="../images/icons/down-arrow-solid-24.png" alt="dropDownArrow">
+                    <ul>
+                        <li>Programming</li>
+                        <li>Businesss & Management</li>
+                        <li>Data Science</li>
+                        <li>Language</li>
+                    </ul>
+                </li>
+            </ul>
+            <ul>
+                <li>Fresher Jobs</li>
+            </ul>
+            <ul>
+                <li><button id="logIn_button">Login</button></li>
+            </ul>
+            <ul>
+                <li>
+                    <span id="register_button">Register<img id="upDownArrow" src="../images/icons/down-arrow-solid-24.png" alt="dropDownArrow"></span>
+                    <ul>
+                        <li>As a student</li>
+                        <li>As an employer</li>
+                    </ul>
+                </li>
+            </ul>
+        </section>
+`
 }
-export default navbar;
+function huberberg(){
+    return `<div id="huberberg">
+    <ul>
+        <li>Internships</li>
+        <li>Online Trainings<span class="hotText">OFFER</span></li>
+        <li>WFH Internships</li>
+        <li>Fressher Jobs</li>
+        <li>Contact US</li>
+    </ul>
+    <div></div>
+    <ul>
+        <li>Register - As a Student</li>
+        <li>Register - As an Employer</li>
+        <li><button id="huberbergLogin_button">Login</button></li>
+    </ul>
+</div>`
+}
+export  {navbar, huberberg};
