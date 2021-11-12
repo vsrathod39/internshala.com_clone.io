13dbfd4e34c5130aeedc584573b3fb58b54c6350
Vikas Kumar <vsrathod39@gmail.com>
2021-11-12 12:56:20 +0530

    slider completed

---
+++ scripts/homepage.js
@@ -0,0 +1,17 @@
+// Home-page scroll slider
+function autoSlider(){
+    let count = 1;
+    let imgesWidth = document.querySelectorAll('.getWidth');
+    setInterval(()=>{
+        if(count === 4){
+            document.getElementById('scrollSlider').scrollLeft = 0;
+            count = 1;
+        }
+        else{
+            document.getElementById('scrollSlider').scrollLeft += imgesWidth[count].clientWidth;
+            console.log(imgesWidth[count].clientWidth)
+            count++;
+        }
+    }, 3000)
+}
+document.addEventListener("DOMContentLoaded", autoSlider());
