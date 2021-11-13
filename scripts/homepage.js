bf8c54e5c90f450d9cb0ed87373eb21daa201c03
Vikas Kumar <vsrathod39@gmail.com>
2021-11-13 07:14:03 +0530

    search engine integrated

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -9,9 +9,22 @@ function autoSlider(){
         }
         else{
             document.getElementById('scrollSlider').scrollLeft += imgesWidth[count].clientWidth;
-            console.log(imgesWidth[count].clientWidth)
             count++;
         }
     }, 3000)
 }
 document.addEventListener("DOMContentLoaded", autoSlider());
+// search box
+document.getElementById('searchInput').onfocus = () => {
+    window.location.href = "searchSec.html"
+}
+// Job type filter
+console.log(document.getElementsByClassName('typeOfJob')[0])
+document.getElementsByClassName('typeOfJob')[0].onclick = () => {
+    // if(localStorage.getItem('internshipDatabase') === null){
+    //     localStorage.setItem('internshipDatabase', JSON.stringify([]));
+    // }
+    // let data = JSON.parse(localStorage.getItem('internshipDatabase'));
+    console.log("hi");
+    console.log(localStorage.getItem("mytime"))
+}
