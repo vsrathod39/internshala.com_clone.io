f682485a92c6529e17dd0ad62f4282a1a32115d5
Shakib40 <shakibjilani@gmail.com>
2021-11-13 17:35:34 +0530

    SearchSec to Filter

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -18,17 +18,14 @@ document.addEventListener("DOMContentLoaded", autoSlider());
 const dataName = document.getElementsByClassName('getDatabaseValue');
 console.log("dataName" , dataName);
 
-
 for(let i=0; i<dataName.length; i++){
     dataName[i].onclick = function () {         
           getvalueData(dataName[i].innerText); 
     }
 }
-
 if(localStorage.getItem("internshipDatabase") == null){
     localStorage.setItem("internshipDatabase" , JSON.stringify([]));
 }
-
 // Adding ID in LocalStorage
 function getvalueData(p){
 
