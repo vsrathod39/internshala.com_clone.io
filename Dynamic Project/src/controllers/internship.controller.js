d1c0c3a658066ef4ec5a56130ee4927c5b0466c4
Shakib40 <shakibjilani@gmail.com>
2021-12-17 17:59:26 +0530

    Filter and InternshipsDetails Page Done

--- Dynamic Project/src/controllers/internship.controller.js
+++ Dynamic Project/src/controllers/internship.controller.js
@@ -5,14 +5,27 @@ const Interns = require("../models/internship.model");
 const router = express.Router();
 
 
-router.get('/', async function (req, res) {
+router.get('/internships', async function (req, res) {
 
     const products = await Interns.find().lean().exec();
     
     return res.render('pages/filter' , {
         products,
     });
+});
 
+// router.get('/internships/:id',  async function (req, res) {
+//     const product = await Interns.findById(req.params.id).lean().exec();
+//     return res.render('pages/internshipsDetails' , {
+//         product,
+//     });
+// });
+
+router.get('/internships/:id',  async function (req, res) {
+    const product = await Interns.findById(req.params.id).lean().exec();
+    return res.render('pages/all' , {
+        product,
+    });
 });
 
 
