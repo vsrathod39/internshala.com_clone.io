f44b1ce31ab3237967ae7c92f29f533f85672620
SKNNT Bohra <90921054+tirthesh-bohra@users.noreply.github.com>
2021-12-17 11:13:24 +0530

    Updated EJS

--- Dynamic Project/src/index.js
+++ Dynamic Project/src/index.js
@@ -1,13 +1,13 @@
 const express = require('express');
-
+const path = require('path');
 const InternsController = require("./controllers/internship.controller");
 
 
 const app = express();
 app.use(express.json());
 
-app.use(express.static("public"));
 app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use("/interns", InternsController);
 
