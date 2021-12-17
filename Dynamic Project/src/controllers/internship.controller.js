84c170312b405f795064ba44cd50c250233748f8
Shakib40 <shakibjilani@gmail.com>
2021-12-17 20:08:02 +0530

    Filter Page

--- Dynamic Project/src/controllers/internship.controller.js
+++ Dynamic Project/src/controllers/internship.controller.js
@@ -6,20 +6,12 @@ const router = express.Router();
 
 
 router.get('/internships', async function (req, res) {
-
-    const products = await Interns.find().lean().exec();
-    
+    const products = await Interns.find().lean().exec();  
     return res.render('pages/filter' , {
         products,
     });
 });
 
-// router.get('/internships/:id',  async function (req, res) {
-//     const product = await Interns.findById(req.params.id).lean().exec();
-//     return res.render('pages/internshipsDetails' , {
-//         product,
-//     });
-// });
 
 router.get('/internships/:id',  async function (req, res) {
     const product = await Interns.findById(req.params.id).lean().exec();
---
+++ Dynamic Project/src/public/js/testing.js
@@ -0,0 +1,3 @@
+// for Testing Purpose
+
+console.log("================================");
