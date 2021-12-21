e674d86ca3e771818f58eea2f27312128a55b5d0
SKNNT Bohra <90921054+tirthesh-bohra@users.noreply.github.com>
2021-12-21 12:30:16 +0530

    Updated File

--- Dynamic Project/src/controllers/auth.controller.js
+++ Dynamic Project/src/controllers/auth.controller.js
@@ -7,45 +7,42 @@ const express = require("express");
 
 require("dotenv").config();
 
-const register = async(req, res) => {
+const register = async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      const Errors = errors.array().map((err) => {
-          return (`{${err.param} : ${err.msg}}`);
-      })
-      return res.send(Errors);
+        const Errors = errors.array().map((err) => {
+            return (`{${err.param} : ${err.msg}}`);
+        })
+        return res.send(Errors);
     }
     try {
-        let user = await User.findOne({email : req.body.email}).lean().exec();
-        if(user)
-        {
-            return res.send({error: "User already exist"});
+        let user = await User.findOne({ email: req.body.email }).lean().exec();
+        if (user) {
+            return res.send({ error: "User already exist" });
         }
-        user = await User.create(req.body); 
-        const token = jwt.sign({ user : user }, process.env.TOKEN_KEY);
+        user = await User.create(req.body);
+        const token = jwt.sign({ user: user }, process.env.TOKEN_KEY);
 
-        res.render("pages/postLoginPage", {token: JSON.stringify(token)});
-    } catch(e) {
-        res.status(500).send({status : "failed"});
+        res.render("pages/postLoginPage", { token: JSON.stringify(token), user: user });
+    } catch (e) {
+        res.status(500).send({ status: "failed" });
     }
 }
 
-const login = async(req, res) => {
+const login = async (req, res) => {
     try {
-        let user = await User.findOne({email : req.body.email});
-        if(!user)
-        {
-            return res.send({error: "Please provide a correct email and password"});
+        let user = await User.findOne({ email: req.body.email });
+        if (!user) {
+            return res.send({ error: "Please provide a correct email and password" });
         }
         const match = await user.checkPassword(req.body.password);
-        if(!match)
-        {
-            return res.send({error: "Please provide a correct email and password"});
+        if (!match) {
+            return res.send({ error: "Please provide a correct email and password" });
         }
-        const token = jwt.sign({ user : user }, process.env.TOKEN_KEY);
-        res.render("pages/postLoginPage", {token: JSON.stringify(token)});
-    } catch(e) {
-        res.status(500).send({status : "failed", message: e.message});
+        const token = jwt.sign({ user: user }, process.env.TOKEN_KEY);
+        res.render("pages/postLoginPage", { token: JSON.stringify(token), user: user });
+    } catch (e) {
+        res.status(500).send({ status: "failed", message: e.message });
     }
 }
 
---
+++ Dynamic Project/src/public/js/jwt.js
@@ -0,0 +1,46 @@
+function login_status_check() {
+    console.log("Ouyt");
+    if (localStorage.getItem("logedin_user")) {
+        const logedin_user = JSON.parse(localStorage.getItem("logedin_user"));
+        console.log(logedin_user);
+        document.getElementById("nameLogo_icon").innerText = logedin_user.user.first_name[0]; // change name logo icon with user first_name first letter
+        document.getElementById("register_btn").style.display = "none"; // hide register button
+        document.getElementById("hamburger_user_name").innerText = logedin_user.user.first_name;
+        document.getElementById("hamburger_user_email").innerText = logedin_user.user.email;
+        document.getElementById("huberberg").style.height = "70vh";
+        console.log("if");
+    } else {
+        document.getElementById("bookmark_icon").style.display = "none"; // hide/shows navbar bookmark icon
+        document.getElementById("message_icon").style.display = "none"; // hide/shows navbar message icon
+        document.getElementById("post_login_views").style.display = "none"; // hide/shows user name logo icon
+        document.getElementById("hamburger_logout").style.display = "none"; // hide/shows logout icon in hamburger
+        document.getElementById("logout_icon").style.display = "none"; // hide/shows logout icon in non-mobile view
+        document.getElementById("hamburger_useer_details").style.display = "none";
+        document.getElementById("hamburger_rating").style.display = "none";
+        document.getElementById("logout_icon").style.display = "none";
+        console.log("else");
+    };
+};
+function desktop_login_status_check() {
+    console.log("Ouyt");
+    if (localStorage.getItem("logedin_user")) {
+        const logedin_user = JSON.parse(localStorage.getItem("logedin_user"));
+        console.log(logedin_user);
+        document.getElementById("nameLogo_icon").innerText = logedin_user.user.first_name[0]; // change name logo icon with user first_name first letter
+        document.getElementById("register_btn").style.display = "none"; // hide register button
+        // document.getElementById("hamburger_user_name").innerText = "Vikas";
+        // document.getElementById("hamburger_user_email").innerText = "v@gmail.com";
+        // document.getElementById("huberberg").style.height = "70vh";
+        console.log("if");
+    } else {
+        document.getElementById("bookmark_icon").style.display = "none"; // hide/shows navbar bookmark icon
+        document.getElementById("message_icon").style.display = "none"; // hide/shows navbar message icon
+        document.getElementById("post_login_views").style.display = "none"; // hide/shows user name logo icon
+        // document.getElementById("hamburger_logout").style.display = "none"; // hide/shows logout icon in hamburger
+        document.getElementById("logout_icon").style.display = "none"; // hide/shows logout icon in non-mobile view
+        // document.getElementById("hamburger_useer_details").style.display = "none";
+        // document.getElementById("hamburger_rating").style.display = "none";
+        document.getElementById("logout_icon").style.display = "none";
+        console.log("else");
+    };
+};
