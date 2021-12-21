2b54c5d03c283e0fddcb1e44f84b9c7aad090bb8
SKNNT Bohra <90921054+tirthesh-bohra@users.noreply.github.com>
2021-12-21 17:33:39 +0530

    Completed

--- Dynamic Project/src/controllers/auth.controller.js
+++ Dynamic Project/src/controllers/auth.controller.js
@@ -32,11 +32,11 @@ const login = async (req, res) => {
     try {
         let user = await User.findOne({ email: req.body.email });
         if (!user) {
-            return res.send({ error: "Please provide a correct email and password" });
+            return res.render("pages/homepage", { alert: "Please provide a correct email and password" });
         }
         const match = await user.checkPassword(req.body.password);
         if (!match) {
-            return res.send({ error: "Please provide a correct email and password" });
+            return res.render("pages/homepage", { alert: "Please provide a correct email and password" });
         }
         const token = jwt.sign({ user: user }, process.env.TOKEN_KEY);
         res.render("pages/postLoginPage", { token: JSON.stringify(token), user: JSON.stringify(user) });
