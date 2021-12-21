09fd45597dc019fbec793eca3cbb22772733b988
Vikas Kumar <vsrathod39@gmail.com>
2021-12-21 13:28:58 +0530

    empty

--- Dynamic Project/src/server.js
+++
@@ -1,7 +0,0 @@
-const  app = require('.');
-const connect = require('./configs/db');
-
-app.listen(2345, async(req, res) => {
-    await connect();
-    console.log("Listening to the PORT 2345");
-});
